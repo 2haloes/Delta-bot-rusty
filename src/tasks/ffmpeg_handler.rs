806b85528946e38ae200370294007146bd2c2f1e
2haloes <rhyshenson@gmail.com>
2024-03-19 16:22:56 +0000

    FFmpeg setup and TTS responses work

--- src/tasks/ffmpeg_handler.rs
+++ src/tasks/ffmpeg_handler.rs
@@ -1,19 +1,38 @@
-use std::{path::PathBuf, process::{Command, Stdio}, u8};
+use std::{io::Write, path::PathBuf, process::{Command, Stdio}, u8};
 use which::which;
+use shell_words::split;
 
-async fn run_ffmpeg(file_input: Vec<u8>, command: String) -> Vec<u8> {
+pub async fn run_ffmpeg(file_input: Vec<u8>, command: String) -> Vec<u8> {
 
     let ffmpeg_location = which("ffmpeg").unwrap_or(PathBuf::default());
 
+    let ffmpeg_input_args: Vec<String> = split(&command).expect("Woopsie");
+
+    let mut ffmpeg_full_args: Vec<String> = Vec::new();
+
+    // This adds in the default args, leaving only the FFmpeg args to be passed to the function
+    ffmpeg_full_args.push("/C".to_owned());
+    ffmpeg_full_args.push(ffmpeg_location.to_str().expect("Test").to_string());
+    ffmpeg_full_args.push("-y".to_owned());
+    ffmpeg_full_args.push("-i".to_owned());
+    ffmpeg_full_args.push("pipe:0".to_owned());
+    ffmpeg_full_args.extend(ffmpeg_input_args);
+    ffmpeg_full_args.push("pipe:1".to_owned());
+
     if ffmpeg_location != PathBuf::default() {
-        let ffmpeg_run = Command::new("cmd")
-            .args([
-                "/C",
-                &(ffmpeg_location.to_str().expect("Test")),
-                &command
-            ])
+        let mut ffmpeg_run = Command::new("cmd")
+            .args(ffmpeg_full_args)
             .stdin(Stdio::piped())
-            .stdout(Stdio::piped());
+            .stdout(Stdio::piped())
+            .spawn()
+            .expect("FFmpeg couldn't start!");
+
+        let mut ffmpeg_stdin = ffmpeg_run.stdin.take().expect("Unable to take FFmpeg stdin!");
+        ffmpeg_stdin.write_all(&file_input).expect("Unable to write file to FFmpeg stdin!");
+        drop(ffmpeg_stdin);
+
+        let ffmpeg_output = ffmpeg_run.wait_with_output().expect("Unable to get data from FFmpeg out");
+        return ffmpeg_output.stdout;
     }
 
    return Vec::new(); 
--- src/tasks/tts.rs
+++ src/tasks/tts.rs
@@ -2,14 +2,18 @@ use async_openai::{types::{CreateSpeechRequestArgs, SpeechModel, Voice}, Client}
 use poise::CreateReply;
 use serenity::all::CreateAttachment;
 
-use crate::{tasks::handle_errors::return_error, Error};
+use crate::{tasks::{ffmpeg_handler::run_ffmpeg, handle_errors::return_error}, Error};
 
 #[poise::command(prefix_command)]
-pub async fn tts(ctx: crate::Context<'_>) -> Result<(), Error> {
+pub async fn tts(
+    ctx: crate::Context<'_>,
+    #[description = "Text to convert to speech"] 
+    tts_string: String
+) -> Result<(), Error> {
     let client = Client::new();
 
     let request = CreateSpeechRequestArgs::default()
-        .input("Today is a wonderful day to build something people love!")
+        .input(tts_string)
         .voice(Voice::Alloy)
         .model(SpeechModel::Tts1)
         .build()?;
@@ -18,9 +22,11 @@ pub async fn tts(ctx: crate::Context<'_>) -> Result<(), Error> {
 
     let attachment: Vec<u8> = response.bytes.to_vec();
 
+    let attachment_processed: Vec<u8>= run_ffmpeg(attachment, "-f matroska -filter_complex \"[0:a]showspectrum=s=hd720:mode=combined:color=intensity:slide=1:scale=cbrt[viz];[viz]format=rgba,geq='p(mod((2*W/(2*PI))*(PI+atan2(0.5*H-Y,X-W/2)),W),H-2*hypot(0.5*H-Y,X-W/2))'\" -c:a mp3".to_string()).await;
+
     let message_builder = CreateReply 
     { 
-        attachments: vec![CreateAttachment::bytes(attachment, "tts_output.ogg")],
+        attachments: vec![CreateAttachment::bytes(attachment_processed, "tts_output.mp4")],
         ..Default::default()
     };
 
