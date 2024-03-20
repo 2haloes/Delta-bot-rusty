06cec23976da4d538a42053aec2a4a6b258558a9
2haloes <rhyshenson@gmail.com>
2024-03-20 12:07:32 +0000

    Improve TTS output message, fix up code and impliment proper error functionality

--- src/tasks/tts.rs
+++ src/tasks/tts.rs
@@ -1,36 +1,48 @@
 use async_openai::{types::{CreateSpeechRequestArgs, SpeechModel, Voice}, Client};
 use poise::CreateReply;
-use serenity::all::CreateAttachment;
+use serenity::all::{CreateAllowedMentions, CreateAttachment};
 
 use crate::{tasks::{ffmpeg_handler::run_ffmpeg, handle_errors::return_error}, Error};
 
-#[poise::command(prefix_command)]
+#[poise::command(prefix_command, slash_command)]
 pub async fn tts(
     ctx: crate::Context<'_>,
     #[description = "Text to convert to speech"] 
     tts_string: String
 ) -> Result<(), Error> {
     let client = Client::new();
+    let requester_id = ctx.author().id;
+    let channel_id = ctx.channel_id();
 
     let request = CreateSpeechRequestArgs::default()
-        .input(tts_string)
-        .voice(Voice::Alloy)
+        .input(tts_string.clone())
+        .voice(Voice::Nova)
         .model(SpeechModel::Tts1)
         .build()?;
 
-    let response = client.audio().speech(request).await?;
+    let response = match client.audio().speech(request).await
+        {
+            Ok(t) => t,
+            Err(e) => return_error(requester_id, channel_id, e.to_string()).await.unwrap(),
+        };
 
     let attachment: Vec<u8> = response.bytes.to_vec();
 
-    let attachment_processed: Vec<u8>= run_ffmpeg(attachment, "-f matroska -filter_complex \"[0:a]showspectrum=s=hd720:mode=combined:color=intensity:slide=1:scale=cbrt[viz];[viz]format=rgba,geq='p(mod((2*W/(2*PI))*(PI+atan2(0.5*H-Y,X-W/2)),W),H-2*hypot(0.5*H-Y,X-W/2))'\" -c:a mp3".to_string()).await;
+    let attachment_processed: Vec<u8>= run_ffmpeg(attachment, "-f matroska -filter_complex \"[0:a]showwaves=s=320x240:colors=White:mode=line'\" -c:a mp3".to_string(), requester_id, channel_id).await;
 
     let message_builder = CreateReply 
     { 
         attachments: vec![CreateAttachment::bytes(attachment_processed, "tts_output.mp4")],
+        allowed_mentions: Some(CreateAllowedMentions::new().replied_user(true)),
+        content: format!("<@{}>\nRequested text: {}", requester_id, tts_string).into(),
         ..Default::default()
     };
 
-    ctx.send(message_builder).await?;
+    match ctx.send(message_builder).await
+        {
+            Ok(t) => t,
+            Err(e) => return_error(requester_id, channel_id, e.to_string()).await.unwrap(),
+        };
 
     Ok(())
 }
